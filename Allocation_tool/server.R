394845eb3368970d0cec9f0f6e0622cc070c5b9d
Juliano Palacios <jepa@Julianos-MacBook-Pro.local>
2021-10-25 15:31:15 -0300

    Fix buggs

--- Allocation_tool/server.R
+++ Allocation_tool/server.R
@@ -35,7 +35,7 @@ state_order <- my_path("D", "Spatial/grid_eez_sw", name = "grid_eez_sw_df.csv", 
     arrange(order)
 
 # ---------------------------- #
-# Grid data from the SW and FP approaches
+# Grid data ####
 # ---------------------------- #
 # Load grid of state waters
 grid_eez_sw_sf <- st_read(my_path("D","Spatial/grid_eez_sw", name = "grid_eez_sw_sf.shp")) %>%
@@ -72,6 +72,21 @@ grids <- my_path("D", "Spatial/grid_eez_fp", name = "grid_eez_fp_df.csv", read =
         spatial = ifelse(is.na(spatial),"sw",spatial)
     ) %>% 
     distinct(.keep_all = T)
+
+
+# ---------------------------- #
+# Quota - reference years ####
+# ---------------------------- #
+# Summer flounder
+# ref_yrs <- c(1980,1989)
+
+# Black sea bass
+# https://static1.squarespace.com/static/511cdc7fe4b00307a2628ac6/t/5176dea1e4b083b631f27236/1366744737698/SFSCBSB_Amend_13_Vol_1compressed.pdf 
+# a) a federal coastwide quota with satate-by-state allocation system Alternative 5f; setion 2.1.5.6 (Pages 3, 14, and 304-Table 5)
+# ref_yrs <- c(1980:2001)
+
+
+# Scup
 
 
 
@@ -120,19 +135,18 @@ shinyServer(function(input, output) {
             )
         
         # For testing
-        # data <- my_path("R","Partial/Interpolation/",name = "tif_Centropristis_striata.csv", read = T)
-        # 
-        # tif_data <- data %>%
-        #     filter(region %in% "Northeast US Fall") %>%
-        #     left_join(grids,
-        #               by = c("index","lat","lon")
-        #     ) %>%
-        #     filter(!is.na(spatial)) %>%
-        #     mutate(
-        #         spatial = ifelse(spatial == "fp","Fishing ports",
-        #                          ifelse(spatial == "sw","State waters","Difference")
-        #         )
-        #     )
+        data <- my_path("R","Partial/Interpolation/",name = "tif_Centropristis_striata.csv", read = T)
+        
+        tif_data <- data %>%
+            left_join(grids,
+                      by = c("index","lat","lon")
+            ) %>%
+            filter(!is.na(spatial)) %>%
+            mutate(
+                spatial = ifelse(spatial == "fp","Fishing ports",
+                                 ifelse(spatial == "sw","State waters","Difference")
+                )
+            )
         
     })
     
@@ -519,7 +533,6 @@ shinyServer(function(input, output) {
     # https://www.littlemissdata.com/blog/prettytables
     # ---------------------------- #
     
-    # output$Allocation_tbl <- renderDataTable({
     output$Allocation_tbl <- renderFormattable({
         
         # Set the filters
@@ -527,272 +540,60 @@ shinyServer(function(input, output) {
         survey <- input$SurveySelection # "Northeast US Fall"
         reg_area <- input$SpatSelection # "State waters"
         
-        total_fited <- tif_data() %>% 
+        total_fited <- tif_data() %>%
             filter(spp %in% species,
                    region %in% survey,
                    # year %in% years,
-                   spatial %in% reg_area) %>% 
-            group_by(year,region,spp,spatial) %>% 
+                   spatial %in% reg_area) %>%
+            group_by(year,region,spp,spatial) %>%
             summarise(total_value = sum(value,na.rm=T),.groups = "drop")
         
         
-        state_fit <- tif_data() %>% 
+        state_table_fit <- tif_data() %>%
             filter(spp %in% species,
                    region %in% survey,
                    # year %in% years,
-                   spatial %in% reg_area) %>% 
-            group_by(state,year,region,spp,spatial) %>% 
-            summarise(state_value = sum(value,na.rm= T), .groups = "drop") %>% 
-            # View()
+                   spatial %in% reg_area) %>%
+            group_by(state,year,region,spp,spatial) %>%
+            summarise(state_value = sum(value,na.rm= T), .groups = "drop") %>%
             left_join(total_fited,
                       by = c("year","region","spp","spatial")) %>%
             mutate(percentage = state_value/total_value*100#,
                    # label = ifelse(year >= 1980 & year <= 2001,"Reference",
                    # ifelse(year > 2001,"Today",NA)
                    # )
-            ) %>% 
-            group_by(state,year,region,spp,spatial) %>% 
-            summarise(mean_per = round(mean(percentage)),.groups = "drop") %>% 
+            ) %>%
+            group_by(state,year,region,spp,spatial) %>%
+            summarise(mean_per = round(mean(percentage)),.groups = "drop") %>%
             #Only show results for spring
-            filter(!is.na(mean_per)) %>% 
+            filter(!is.na(mean_per)) %>%
             select(state,year,mean_per) %>%
             spread(year,mean_per) %>%
-            mutate(
-                Change = ifelse(`1973` > `2019`,"Increase","Decrease")
-            ) %>% 
-            select(state,`1973`:`1977`,`2012`:`2019`)
+            mutate(Change = ifelse(`1973` < `2019`,"Increase","Decrease")) %>%
+            left_join(state_order) %>% 
+            arrange(order) %>% 
+            mutate(State = paste0(state," (",abrev,")")) %>% 
+            select(1:6,42:46,Change)
         
+        
+        
+        customGreen0 = "#DeF7E9"
+        
+        customGreen = "#71CA97"
         
         #Print format table
-        formattable(state_fit, 
+        formattable(state_table_fit,
                     align =c("l",rep("c",10),"r"),
                     list(
                         # area(`1973`:`2019`) ~ normalize_bar("pink", 0.2),
-                        Change = formatter("span", 
-                                           x ~ icontext(ifelse(x == "Increase", "arrow-up", "arrow-down"), ifelse(x == "Increase", "Increase", "Decrease")), 
+                        `1973`= color_bar(customGreen),
+                        `2019` = color_bar(customGreen),
+                        Change = formatter("span",
+                                           x ~ icontext(ifelse(x == "Increase", "arrow-up", "arrow-down"), 
+                                                        ifelse(x == "Increase", "Increase", "Decrease")),
                                            style = x ~ style(color = ifelse(x == "Increase", "green", "red")))
                     )
-        ) # close formattable
-    })
-    
-    
-    
-    
-    # ## ---------------------------- #
-    # # Dynamic plots (plotly) ####
-    # ## ---------------------------- #
-    # output$areaPlot <- renderPlotly({
-    #     
-    #     # ---------------------------- #
-    #     ## Area plot ######
-    #     # ---------------------------- #
-    #     
-    #     if(input$PlotStyle == 4){
-    #         # Set the filters
-    #         species <- input$SppSelection #"Gadus morhua"
-    #         survey <- input$SurveySelection #"Northeast US Fall"
-    #         years <- seq(input$YearSelection[1],input$YearSelection[2],1) #seq(1971,2019,1)
-    #         # rmena_value <- ifelse(input$Rmean == 0,)
-    #         
-    #         
-    #         total_fited <- tif_data() %>% 
-    #             filter(spp %in% species,
-    #                    region %in% survey,
-    #                    year %in% years
-    #             ) %>% 
-    #             group_by(year,region) %>% 
-    #             summarise(total_value = sum(value,na.rm=T), .groups = "drop")
-    #         
-    #         # group by state
-    #         state_fit <- tif_data() %>% 
-    #             group_by(state,year,region) %>% 
-    #             summarise(state_value = sum(value,na.rm= T), .groups = "drop") %>% 
-    #             left_join(total_fited,
-    #                       by = c("year","region")) %>%
-    #             mutate(percentage = state_value/total_value*100) %>% 
-    #             group_by(state,region) %>% 
-    #             mutate(RMean = zoo::rollmean(x = percentage,
-    #                                          input$Rmean,
-    #                                          fill = NA,
-    #                                          na.rm=T)
-    #             )
-    #         
-    #         p <- ggplot(state_fit) +
-    #             geom_area(
-    #                 aes(
-    #                     x = year,
-    #                     y = round(RMean),
-    #                     fill = state
-    #                 )
-    #             ) +
-    #             ylab("Percentage (%)") +
-    #             scale_fill_manual(values = state_pallet) +
-    #             MyFunctions::my_ggtheme_p() +
-    #             theme(legend.position = "top") #+
-    #         # facet_wrap(~region, ncol = 1)
-    #         
-    #         ggplotly(p,
-    #                  dynamicTicks = TRUE,
-    #                  height = 600) %>% 
-    #             layout(hovermode = "x") %>% 
-    #             # add_trace() %>% 
-    #             rangeslider()
-    #     }else{
-    #         # ---------------------------- #
-    #         # Distribution proportion map ######
-    #         # ---------------------------- #
-    #         if(input$PlotStyle == 6){
-    #             
-    #             periods <-tibble(
-    #                 order = c(rep("a",12),
-    #                           rep("b",12),
-    #                           rep("c",12),
-    #                           rep("d",11)
-    #                 ),
-    #                 label = c(rep("1973-1984",12),
-    #                           rep("1985-1996",12),
-    #                           rep("1997-2008",12),
-    #                           rep("2009-2019",11)
-    #                 ),
-    #                 year = c(seq(1973,1984,1),
-    #                          seq(1985,1996,1),
-    #                          seq(1997,2008,1),
-    #                          seq(2009,2019,1)
-    #                 )
-    #             )
-    #             
-    #             total_fited <- tif_data() %>% 
-    #                 group_by(year,region,spp) %>% 
-    #                 summarise(total_value = sum(value,na.rm=T),.groups = "drop")
-    #             
-    #             
-    #             state_fit <- tif_data() %>% 
-    #                 group_by(state,year,region,spp) %>% 
-    #                 summarise(state_value = sum(value,na.rm= T), .groups = "drop") %>% 
-    #                 left_join(total_fited,
-    #                           by = c("year","region","spp")) %>%
-    #                 mutate(percentage = state_value/total_value*100) %>% 
-    #                 left_join(periods,
-    #                           by = "year") %>% 
-    #                 group_by(state,order,label,region,spp) %>% 
-    #                 summarise(mean_per = round(mean(percentage)),.groups = "drop") %>% 
-    #                 #Only show results for spring
-    #                 filter(str_detect(region,"Spring")) %>% 
-    #                 mutate(spp = gsub(" ","\n",spp))
-    #             
-    #             # The plot
-    #             p <- us_map %>% 
-    #                 filter(ID %in% c("maine", "new hampshire", "massachusetts", "connecticut", 
-    #                                  "rhode island", "new york", "new jersey", "delaware", "maryland",
-    #                                  "virginia", "north carolina")) %>% 
-    #                 rename(state = ID) %>% 
-    #                 left_join(state_fit,
-    #                           by = "state") %>% 
-    #                 ggplot() +
-    #                 geom_sf(aes(fill = mean_per,
-    #                             text = paste(state, mean_per,"% of proportion")
-    #                 )) +
-    #                 viridis::scale_fill_viridis("Average proportion\nper State", alpha = 0.8) +
-    #                 facet_wrap(~label, nrow = 2) +
-    #                 labs(x = "Longitude", 
-    #                      y = "Latitude") +
-    #                 my_ggtheme_p(facet_tx_s = 18,
-    #                              leg_pos = "bottom",
-    #                              axx_tx_ang = 45,
-    #                              ax_tx_s = 12,
-    #                              ax_tl_s = 18,
-    #                              hjust = 1) +
-    #                 theme(legend.key.width = unit(1,"line"))
-    #             
-    #             ggplotly(p,
-    #                      tooltip = "text",
-    #                      dynamicTicks = FALSE) 
-    #             
-    #         }
-    #     }
-    #     
-    # })
-    
-    
-    # ---------------------------- #
-    
-    
-    
-    
-    ### Old code
-    # ---------------------------- #
-    ## Latitudinal plot ######
-    # ---------------------------- #
-    # if(input$PlotStyle == 2){
-    #     
-    #     # Set the plot data
-    #     avr_lat <- plot_data %>% 
-    #         filter(wtcpue > 0) %>% 
-    #         group_by(year,spp) %>%
-    #         summarise(mean_lat = mean(lat,na.rm = T), .groups = "drop") %>% 
-    #         group_by(spp) %>%
-    #         mutate(Rmean = zoo::rollmean(x = mean_lat,
-    #                                      5,
-    #                                      align = "right",
-    #                                      fill = mean_lat)
-    #         )
-    #     
-    #     ggplot(avr_lat) +
-    #         geom_line(
-    #             aes(
-    #                 x = as.numeric(year),
-    #                 y = Rmean
-    #             )
-    #         ) +
-    #         geom_point(
-    #             aes(
-    #                 x = as.numeric(year),
-    #                 y = mean_lat
-    #             )
-    #         ) +
-    #         xlab("Year") +
-    #         ylab("Latitude") +
-    #         MyFunctions::my_ggtheme_p() +
-    #         ggtitle(paste("Average shift of",unique(plot_data$spp),"(5 years running mean)"))
-    #     
-    # }else{
-    
-    # ---------------------------- #
-    # Density map ######
-    # ---------------------------- #
-    # if(input$PlotStyle == 1){
-    
-    # ggplot(us_map) +
-    #     geom_sf() +
-    #     geom_point(data = subset(plot_data, wtcpue = 0),
-    #                aes(
-    #                    x = lon,
-    #                    y = lat
-    #                ),
-    #                color = "grey95",
-    #                size = 1
-    #     ) +
-    #     geom_point(data = subset(plot_data, wtcpue > 0),
-    #                aes(
-    #                    x = lon,
-    #                    y = lat,
-    #                    color = log10(wtcpue)
-    #                ),
-    #                size = 1
-    #     ) +
-    #     scale_color_distiller(palette = "Spectral",
-    #                           guide_legend(title = "WCPUE per Haul (log10)")) +
-    #     coord_sf(xlim = c(-76, -65),ylim = c(35, 45)) +
-    #     MyFunctions::my_ggtheme_m(leg_pos = "right") +
-    #     facet_wrap(~region) +
-    #     ggtitle(paste(min(years),max(years)))
-    # }else{
-    
-    # ---------------------------- #
-    # DIstribution map ######
-    # ---------------------------- #
-    # if(input$PlotStyle == 3){
-    
-    # The actual map
+        )
+    })  # close formattable
     
 }) # app closure
